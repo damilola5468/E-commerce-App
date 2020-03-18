e70278e7f60086cce28af90451451ab58f2135c0
Damilola <lorlahstrings@gmail.com>
2020-03-18 15:10:54 +0100

    aplha base

--- api/engine.js
+++ api/engine.js
@@ -22,15 +22,19 @@ var mysqlConnection = mysql.createConnection({
   database: "agro"
 });
 
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
+});
 
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
-  });
-}
+// app.use(express.static(path.join(__dirname, 'build')));
+// app.get('/', function (req, res) {
+// app.get('/*', function (req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//  });
 
 mysqlConnection.connect(err => {
   if (!err) {
