97ace38f2bc0b4a743a7b158fc1b6fcddcd4fba0
Damilola <38119651+damilola5468@users.noreply.github.com>
2020-09-28 07:55:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -49,232 +49,232 @@ app.get("/", (req, res) => {
 });
 
 
-// app.get("/hello", (req, res) => {
-//   res.send("Hello World");
-// });
+app.get("/hello", (req, res) => {
+  res.send("Hello World");
+});
 
-// app.post("/c/products", (req, res) => {
-//   var sql = "SELECT * FROM Products ";
-//   client.query(sql, (err, result) => {
-//     console.log(err);
-//     let products = [],
-//       id = null;
-//     let cart = JSON.parse(req.body.cart);
-//     if (!cart) return res.json(products);
-//     for (var i = 0; i < result.length; i++) {
-//       id = result[i].id.toString();
-//       if (cart.hasOwnProperty(id)) {
-//         result[i].qty = cart[id];
-//         products.push(result[i]);
-//       }
-//     }
-//     return res.json(products);
-//   });
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.post("/c/products", (req, res) => {
+  var sql = "SELECT * FROM Products ";
+  client.query(sql, (err, result) => {
+    console.log(err);
+    let products = [],
+      id = null;
+    let cart = JSON.parse(req.body.cart);
+    if (!cart) return res.json(products);
+    for (var i = 0; i < result.length; i++) {
+      id = result[i].id.toString();
+      if (cart.hasOwnProperty(id)) {
+        result[i].qty = cart[id];
+        products.push(result[i]);
+      }
+    }
+    return res.json(products);
+  });
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.post("/api/auth", (req, res) => {
-//   var sql = "SELECT * FROM custormers";
-//   client.query(sql, (err, result) => {
-//     console.log(err);
-//     let user = result.filter(user => {
-//       return (
-//         user.email === req.body.email && user.password === req.body.password
-//       );
-//     });
-//     if (user.length) {
-//       let token_payload = {
-//         email: user[0].email,
-//         password: user[0].password
-//       };
-//       let token = jwt.sign(token_payload, "jwt_secret_password", {
-//         expiresIn: "2h"
-//       });
-//       let response = {
-//         message: "Token Created, Authentication Successful!",
-//         token: token
-//       };
+app.post("/api/auth", (req, res) => {
+  var sql = "SELECT * FROM custormers";
+  client.query(sql, (err, result) => {
+    console.log(err);
+    let user = result.filter(user => {
+      return (
+        user.email === req.body.email && user.password === req.body.password
+      );
+    });
+    if (user.length) {
+      let token_payload = {
+        email: user[0].email,
+        password: user[0].password
+      };
+      let token = jwt.sign(token_payload, "jwt_secret_password", {
+        expiresIn: "2h"
+      });
+      let response = {
+        message: "Token Created, Authentication Successful!",
+        token: token
+      };
 
-//       return res.status(200).json(response);
-//     } else {
-//       return res.status("409").json("Authentication failed. User not found.");
-//     }
-//   });
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+      return res.status(200).json(response);
+    } else {
+      return res.status("409").json("Authentication failed. User not found.");
+    }
+  });
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/Products", function(req, res) {
-//   var sql = "SELECT * FROM Products ORDER BY id DESC;";
-//   client.query(sql, (err, result) => {
-//     if (err) console.log(err);
-//     console.log(result);
-//     res.send(result);
-//   });
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/Products", function(req, res) {
+  var sql = "SELECT * FROM Products ORDER BY id DESC;";
+  client.query(sql, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+    res.send(result);
+  });
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/Type", function(req, res) {
-//   var sql = "SELECT * FROM type;";
-//   client.query(sql, (err, result) => {
-//     if (err) console.log(err);
-//     console.log(result);
-//     res.send(result);
-//   });
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/Type", function(req, res) {
+  var sql = "SELECT * FROM type;";
+  client.query(sql, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+    res.send(result);
+  });
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/SubCat/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM subcategory WHERE cat_id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/SubCat/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM subcategory WHERE cat_id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/SububCat/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM sububcategory WHERE sub_category_id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/SububCat/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM sububcategory WHERE sub_category_id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/Sububproduct/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM products WHERE subub_category_id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/Sububproduct/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM products WHERE subub_category_id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/allSububproduct/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM products WHERE subub_category_id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/allSububproduct/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM products WHERE subub_category_id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/subSubCat/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM sububcategory WHERE sub_category_id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/subSubCat/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM sububcategory WHERE sub_category_id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/SubCatpro/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM products WHERE sub_category =" +
-//       req.params.id +
-//       " ORDER BY id DESC;",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/SubCatpro/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM products WHERE sub_category =" +
+      req.params.id +
+      " ORDER BY id DESC;",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/Sububpro/:id", (req, res) => {
-//   client.query(
-//     "SELECT * FROM products WHERE sub_category =" +
-//       req.params.id +
-//       " ORDER BY id DESC;",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/Sububpro/:id", (req, res) => {
+  client.query(
+    "SELECT * FROM products WHERE sub_category =" +
+      req.params.id +
+      " ORDER BY id DESC;",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.post("/Products/", function(req, res) {
-//   var sql = "SELECT * FROM products ORDER BY id DESC;";
-//   client.query(sql, (err, result) => {
-//     if (err) console.log(err);
-//     console.log(result);
-//     res.send(result);
-//   });
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.post("/Products/", function(req, res) {
+  var sql = "SELECT * FROM products ORDER BY id DESC;";
+  client.query(sql, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+    res.send(result);
+  });
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/product/:name", (req, res) => {
-//   client.query(
-//     "SELECT * FROM products WHERE name LIKE '%" + [req.params.name] + "%';",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/product/:name", (req, res) => {
+  client.query(
+    "SELECT * FROM products WHERE name LIKE '%" + [req.params.name] + "%';",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/pro/:id", (req, res) => {
-//   client.query(
-//     "Delete FROM products WHERE id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/pro/:id", (req, res) => {
+  client.query(
+    "Delete FROM products WHERE id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// //
+//
 
-// app.get("/product_id/:id", (req, res) => {
-//   client.query(
-//     "Select * FROM products WHERE id =" + req.params.id + ";",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/product_id/:id", (req, res) => {
+  client.query(
+    "Select * FROM products WHERE id =" + req.params.id + ";",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
-// app.get("/pay/:email", (req, res) => {
-//   client.query(
-//     "SELECT * FROM custormers WHERE email ='" + req.params.email + "';",
-//     (err, rows, fields) => {
-//       if (!err) res.send(rows);
-//       else console.log(err);
-//     }
-//   );
-//   // client.end();
-//   // mysqlConnection.end();
-// });
+app.get("/pay/:email", (req, res) => {
+  client.query(
+    "SELECT * FROM custormers WHERE email ='" + req.params.email + "';",
+    (err, rows, fields) => {
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+  // client.end();
+  // mysqlConnection.end();
+});
 
 // app.get("/cat/:id", (req, res) => {
 //   client.query(
